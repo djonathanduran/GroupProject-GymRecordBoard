810cf431e4b18439f6cbbc4e209446a71b3557c6
Nicolas Puente <nicolaspeaceramirez@gmail.com>
2022-03-31 12:25:33 -0500

    changing mysqlconnection password

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -7,7 +7,7 @@ class MySQLConnection:
         # change the user and password as needed
         connection = pymysql.connect(host='localhost',
                                      user='root',
-                                     password='Nebuchadnezzar1789',
+                                     password='rootroot',
                                      db=db,
                                      charset='utf8mb4',
                                      cursorclass=pymysql.cursors.DictCursor,
